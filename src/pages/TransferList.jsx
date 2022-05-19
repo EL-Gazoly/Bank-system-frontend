cd83e60210c0b5f5f08b5a4bf650017de3ba141e
Adham ElGazoly <adham20191701021@cis.asu.edu.eg>
2022-05-19 21:49:19 +0200

    Front-end

--- src/pages/TransferList.jsx
+++ src/pages/TransferList.jsx
@@ -1,7 +1,9 @@
 import axios from "../Features/axios";
 import React , {useState , useEffect} from 'react'
 import DisplayTransfers from "../components/DisplayTrasfers";
-
+import useFetch from '../Features/useFetch'
+import NotFound from "./notFound";
+import Loading from "./loading";
  
 function TransferList() {
     const [transfers , setTransfers ]= useState([]);
@@ -14,11 +16,15 @@ function TransferList() {
       }
       fetchData();
     },[]);
+    const { data, isPending, error } = useFetch('https://team-el-gazoly-bank-system-main-k3vbm4kpjq-wm.a.run.app/api/transfer/'+'table')
+    
 
   return (
     <div>
-
+       {isPending && <Loading />}
+      {error && <NotFound />}
       <DisplayTransfers transfers={transfers} />
+      
     </div>
   )
 }
--- src/pages/usersList.jsx
+++ src/pages/usersList.jsx
@@ -1,7 +1,10 @@
 import axios from "../Features/axios";
 import React , {useState , useEffect} from 'react'
 import DispalyUsers from "../components/DisplayUsers";
-
+import useFetch from '../Features/useFetch'
+import NotFound from "./notFound";
+import Loading from "./loading";
+ 
 
 function UsersList() {
     const [users , setUsers ]= useState([]);
@@ -14,9 +17,12 @@ function UsersList() {
       }
       fetchData();
     },[]);
+    const { data, isPending, error } = useFetch('https://team-el-gazoly-bank-system-main-k3vbm4kpjq-wm.a.run.app/api/users/all')
 
   return (
     <div>
+      {isPending && <Loading />}
+      {error && <NotFound />}
       <DispalyUsers users={users} />
     </div>
   )
